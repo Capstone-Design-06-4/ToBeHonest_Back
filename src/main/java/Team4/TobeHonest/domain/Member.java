0eca8a84f864e089f288c1e0eb10baa9ddff3af5
MJ <alswns2631@cau.ac.kr>
2023-10-01 23:40:03 +0900

    fix: member엔티티 수정 및 userDetailsImpl삭지

--- src/main/java/Team4/TobeHonest/domain/Member.java
+++ src/main/java/Team4/TobeHonest/domain/Member.java
@@ -4,19 +4,20 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
 
 @Entity
 @Getter
 @NoArgsConstructor
-public class Member {
+public class Member implements UserDetails {
 
     @Id
     @GeneratedValue
@@ -43,20 +44,51 @@ public class Member {
         this.birthDate = birthDate;
     }
 
-    public FriendWith addFriend(Member friend){
-
-        return new FriendWith(this, friend);
-    }
-
-//    로그인 해시값 찾기
+    //    로그인 해시값 찾기
     public static String hashPassword(String password, PasswordEncoder passwordEncoder) {
         return passwordEncoder.encode(password);
 
+    }
+
+    public FriendWith addFriend(Member friend) {
+        return new FriendWith(this, friend);
     }
 
     public boolean checkPassword(String plainPassword, PasswordEncoder passwordEncoder) {
         return passwordEncoder.matches(plainPassword, this.password);
     }
 
+
+    @Override
+    public String getPassword() {
+        return this.getPassword();
+    }
+
+    @Override
+    public String getUsername() { return this.getName(); }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
 
 }
