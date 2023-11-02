8027c0697e065164ef07466331d0c4b4ac97df7b
MJ <alswns2631@cau.ac.kr>
2023-11-02 17:08:08 +0900

    fix: delete friend: post -> delete로 변경

--- src/main/java/Team4/TobeHonest/controller/MemberController.java
+++ src/main/java/Team4/TobeHonest/controller/MemberController.java
@@ -17,10 +17,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -78,7 +75,7 @@ public class MemberController {
 
 
 
-    @GetMapping("/friends/delete/{friendId}")
+    @DeleteMapping("/friends/delete/{friendId}")
     public ResponseEntity<String> deleteFriend(@PathVariable Long friendId,
                                             @AuthenticationPrincipal UserDetails userDetails) {
         Member member = (Member) userDetails;
